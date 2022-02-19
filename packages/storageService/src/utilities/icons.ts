a65b22a49e125c80163c91bb81d07a9d559f1712
Luciano Frizzera <lucaju@me.com>
2022-02-18 21:02:44 -0500

    (storage service)refactor: 💡 simplify getIcon

--- packages/storageService/src/utilities/icons.ts
+++ packages/storageService/src/utilities/icons.ts
@@ -3,15 +3,23 @@ import ComputerIcon from '@mui/icons-material/Computer';
 import ContentPasteIcon from '@mui/icons-material/ContentPaste';
 import FingerprintIcon from '@mui/icons-material/Fingerprint';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import type { OverridableComponent } from '@mui/material/OverridableComponent';
+import type { SvgIconTypeMap } from '@mui/material/SvgIcon';
 import { Gitlab } from 'mdi-material-ui';
 
-const icons: Map<string, any> = new Map();
-icons.set('github', GitHubIcon);
-icons.set('gitlab', Gitlab);
-icons.set('fingerprint', FingerprintIcon);
-icons.set('cloud', CloudQueueIcon);
-icons.set('computer', ComputerIcon);
-icons.set('paste', ContentPasteIcon);
+const icons: Map<
+  string,
+  OverridableComponent<SvgIconTypeMap<{}, 'svg'>> & {
+    muiName: string;
+  }
+> = new Map([
+  ['github', GitHubIcon],
+  ['gitlab', Gitlab],
+  ['fingerprint', FingerprintIcon],
+  ['cloud', CloudQueueIcon],
+  ['computer', ComputerIcon],
+  ['paste', ContentPasteIcon],
+]);
 
 export const getIcon = (name: string): any => {
   const icon = icons.get(name);
