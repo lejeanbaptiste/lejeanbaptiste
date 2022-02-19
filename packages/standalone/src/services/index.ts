39e385f6b68f60108c68543cdb80d1bee873e792
Luciano Frizzera <lucaju@me.com>
2022-02-18 21:25:19 -0500

    (standalone)fix: 🐛 remove unecessary service endpoint to github gitlab orcidid

--- packages/standalone/src/services/index.ts
+++ packages/standalone/src/services/index.ts
@@ -1,11 +1,12 @@
 import type { IdentityProvider, AuthenticateProp } from './IdentityProvider';
-import type { StorageProvider } from './StorageProvider';
-import { GithubIdentityProvider, GithubStorageProvider } from './github/github';
-import { GitlabIdentityProvider, GitlabStorageProvider } from './gitlab/gitlab';
+import { GithubIdentityProvider } from './github/github';
+import { GitlabIdentityProvider } from './gitlab/gitlab';
 import { OrcidIdentityProvider } from './orcid/orcid';
+import type { IdentityProvider as IdentityProviderType, StorageProvider } from '@src/@types/types';
 
-export const supportedIdentityProviders = ['orcid', 'gitlab', 'github'];
-export const suportedStorageProviders = ['gitlab', 'github'];
+// export const supportedIdentityProviders: IdentityProviderType[] = ['orcid', 'gitlab', 'github'];
+export const supportedIdentityProviders: IdentityProviderType[] = ['gitlab', 'github'];
+export const suportedStorageProviders: StorageProvider[] = ['gitlab', 'github'];
 
 export const setIndentityProvider = ({
   IDPTokens,
@@ -13,7 +14,7 @@ export const setIndentityProvider = ({
   userId,
   userName,
 }: AuthenticateProp) => {
-  if (!providerName || !supportedIdentityProviders.includes(providerName)) {
+  if (!providerName || !supportedIdentityProviders.includes(providerName as IdentityProviderType)) {
     throw new Error('Identity Provider not supported');
   }
 
@@ -25,27 +26,6 @@ export const setIndentityProvider = ({
   else throw new Error('Identity Provider not supported');
 
   provider.authenticate({ IDPTokens, userName, userId });
-
-  return provider;
-};
-
-export const setStorageProvider = ({
-  access_token,
-  providerName,
-  userId,
-  userName,
-}: AuthenticateProp) => {
-  if (!providerName || !suportedStorageProviders.includes(providerName)) {
-    throw new Error('Storage Provider not supported');
-  }
-
-  let provider: StorageProvider;
-
-  if (providerName === 'github') provider = GithubStorageProvider;
-  else if (providerName === 'gitlab') provider = GitlabStorageProvider;
-  else throw new Error('Storage Provider not supported');
-
-  provider.authenticate({ access_token, userName, userId });
 
   return provider;
 };
