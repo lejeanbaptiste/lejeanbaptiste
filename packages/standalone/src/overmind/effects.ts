7a362c09f602ef3d694a2d8cb8e3cebfb412fe00
Luciano Frizzera <lucaju@me.com>
2022-03-02 18:02:24 -0500

    (standalone)fix: 🐛 remove extra '/' from NSSI URL

--- packages/standalone/src/overmind/effects.ts
+++ packages/standalone/src/overmind/effects.ts
@@ -4,7 +4,7 @@ const LINK_ACCOUNTS_CALLBACK_URL = `${window.location.origin}/link-accounts`;
 
 const KEYCLOACK_DEV_URL = 'https://keycloak.dev.lincsproject.ca';
 // const NSSI_REVIEW_URL = 'https://api.16893933-review-243-workfl-r3lefc.dev.lincsproject.ca/api/';
-const NSSI_STAGE_URL = 'https://api.nssi.stage.lincsproject.ca/api/';
+const NSSI_STAGE_URL = 'https://api.nssi.stage.lincsproject.ca/api';
 
 export const LincsApi = {
   getExternalIDPTokens: async (
