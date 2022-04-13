26c93e565dd8abb8ab0fdf38ab4d5926646376a8
Luciano Frizzera <lucaju@me.com>
2022-04-12 22:48:41 -0400

    (standalone)style: 💄 rename hook files

--- packages/standalone/src/components/Storage.tsx
+++ packages/standalone/src/components/Storage.tsx
@@ -1,6 +1,6 @@
 import type { Resource } from '@cwrc/leafwriter-storage-service';
 import { Backdrop, LinearProgress } from '@mui/material';
-import { usePermalink } from '@src/hooks/permalink';
+import { usePermalink } from '@src/hooks/usePermalink';
 import { useActions, useAppState } from '@src/overmind';
 import React, { FC, Suspense, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router';
