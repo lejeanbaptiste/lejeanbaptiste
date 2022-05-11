9b7ae1f0c810c6b840aba148c47018f8778cb495
Luciano Frizzera <lucaju@me.com>
2022-05-10 23:24:27 -0400

    (standalone)fix: 🐛 replace chalk with kleur

--- packages/standalone/server/index.ts
+++ packages/standalone/server/index.ts
@@ -1,7 +1,7 @@
-import chalk from 'chalk';
+import kleur from 'kleur';
 import app from './server';
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(chalk.bgGreen.black(`\n Server listening on port ${port}! \n`));
+  console.log(kleur.bgGreen().black(`\n Server listening on port ${port}! \n`));
 });
