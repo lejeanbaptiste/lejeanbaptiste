a6f095a815074c87edc187300a7db9fe953cc4dc
Luciano Frizzera <lucaju@me.com>
2022-06-14 18:29:14 -0300

    feat(commons): 🎸 Redesign storage panel
    Add scheme type and date to recent files

--- packages/commons/src/views/home/storagePanel/Recent.tsx
+++ packages/commons/src/views/home/storagePanel/Recent.tsx
@@ -1,62 +1,35 @@
-import { loadDocument } from '@cwrc/leafwriter-storage-service';
-import { Button, Stack, Typography } from '@mui/material';
-import type { Resource } from '@src/@types/types';
-import { usePermalink } from '@src/hooks/usePermalink';
-import { useActions, useAppState } from '@src/overmind';
-import React, { FC } from 'react';
+import { Divider, Stack, Typography } from '@mui/material';
+import { useAppState } from '@src/overmind';
+import React, { type FC } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useNavigate } from 'react-router';
+import RecentFile from './RecentFile';
 
 const Recent: FC = () => {
-  const navigate = useNavigate();
   const { recentDocuments } = useAppState().storage;
   const { t } = useTranslation();
-  const { getStorageProviderAuth, setResource } = useActions();
-  const { setPermalink } = usePermalink();
-
-  const handleClick = async (resource: Resource) => {
-    if (!resource.provider) return;
-
-    const providerAuth = getStorageProviderAuth(resource.provider);
-    if (!providerAuth) return;
-
-    const document = await loadDocument(providerAuth, resource);
-    if (!document || 'error' in document || !document.content || !document.url) {
-      return;
-    }
-
-    setResource(document);
-    setPermalink(document);
-    navigate('/edit', { replace: true });
-  };
 
   return (
-    <Stack spacing={1} sx={{ maxWidth: 250 }}>
-      <Typography
-        align="center"
-        component="h5"
-        mb={2}
-        sx={{ fontWeight: 700, textTransform: 'uppercase' }}
-        variant="h6"
-      >
+    <Stack spacing={1} sx={{ width: 320 }}>
+      <Typography sx={{ fontWeight: 700, letterSpacing: '.15rem', textTransform: 'uppercase' }}>
         {t('home:recent')}
       </Typography>
-      <Stack>
-        {recentDocuments.map((resource, index) => (
-          <Button
-            key={index}
-            color="inherit"
-            onClick={() => handleClick(resource)}
-            sx={{
-              textTransform: 'unset',
-              whiteSpace: 'nowrap',
-              overflow: 'hidden',
-              textOverflow: 'ellipsis',
-            }}
-          >
-            {resource.filename}
-          </Button>
-        ))}
+      <Stack direction="row" spacing={2} minHeight={145}>
+        <Divider
+          flexItem
+          orientation="vertical"
+          sx={{ borderColor: '#999', ml: 0.5, boxShadow: '2px 0px 2px 0px rgb(0 0 0 / 15%)' }}
+        />
+        {recentDocuments.length === 0 ? (
+          <Typography sx={{ fontSize: '1.1rem', fontWeight: 700, opacity: 0.3 }} variant="h6">
+            Recent documents are listed here
+          </Typography>
+        ) : (
+          <Stack spacing={1} width={275}>
+            {recentDocuments.map((resource, index) => (
+              <RecentFile key={index} resource={resource} />
+            ))}
+          </Stack>
+        )}
       </Stack>
     </Stack>
   );
--- packages/commons/src/views/home/storagePanel/Templates.tsx
+++ packages/commons/src/views/home/storagePanel/Templates.tsx
@@ -1,5 +1,6 @@
 import { Button, Divider, Stack, Typography, useMediaQuery, useTheme } from '@mui/material';
 import { useActions, useAppState } from '@src/overmind';
+import { getIcon } from '@src/utilities/icons';
 import React, { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router';
@@ -12,39 +13,57 @@ const Templates: FC = () => {
   const { breakpoints, palette } = useTheme();
   const isMobile = useMediaQuery(breakpoints.down('sm'));
 
-  const handleClick = async (url: string) => {
+  const handleClick = async (title: string, url: string) => {
     const documentString = await loadTemplate(url);
     setResource({ content: documentString });
-    navigate('/edit', { replace: true });
+    navigate(`/edit?template=${title}`, { replace: true });
   };
 
   return (
-    <Stack spacing={1} sx={{ maxWidth: isMobile ? 'auto' : 170 }}>
-      <Typography
-        align="center"
-        component="h5"
-        mb={2}
-        sx={{ fontWeight: 700, textTransform: 'uppercase' }}
-        variant="h6"
-      >
+    <Stack
+      direction="row"
+      spacing={3}
+      alignItems="center"
+      sx={{
+        width: 600,
+        py: 0.75,
+        px: 2,
+        backgroundColor: palette.mode === 'dark' ? palette.grey[900] : palette.grey[50],
+        borderRadius: 3,
+      }}
+    >
+      <Typography sx={{ fontWeight: 700, letterSpacing: '.15rem', textTransform: 'uppercase' }}>
         {t('home:templates')}
       </Typography>
-      <Typography align="center" component="h6" variant="subtitle2">
-        TEI
-      </Typography>
-      <Stack direction="row" flexWrap="wrap" justifyContent="center">
-        {templates.map(({ title, url }) => (
-          <Button
-            key={title}
-            color="secondary"
-            onClick={() => handleClick(url)}
-            size="small"
-            sx={{ m: 0.5 }}
-            variant="outlined"
-          >
-            {title}
-          </Button>
-        ))}
+      <Divider
+        flexItem
+        orientation="vertical"
+        sx={{ borderColor: '#999', boxShadow: '2px 0px 2px 0px rgb(0 0 0 / 15%)' }}
+      />
+      <Stack direction="row" justifyContent="center" spacing={3}>
+        {templates.map(({ icon, title, url }) => {
+          const Icon = getIcon(icon);
+          return (
+            <Button
+              key={title}
+              color="primary"
+              onClick={() => handleClick(title, url)}
+              size="small"
+              startIcon={<Icon fontSize="inherit" />}
+              sx={{
+                color: 'inherit',
+                px: 1,
+                '&:hover': {
+                  color: palette.primary.main,
+                  backgroundColor: 'transparent',
+                },
+              }}
+              variant="text"
+            >
+              {title}
+            </Button>
+          );
+        })}
       </Stack>
     </Stack>
   );
