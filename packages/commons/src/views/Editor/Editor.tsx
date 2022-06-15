a7c58019fca429d8cb56a219d39871672e57c70b
Luciano Frizzera <lucaju@me.com>
2022-06-15 00:12:41 -0300

    feat(commons): 🎸 new way to load leaf-writer
    Leaf-writer is a instanciable class. It requires the content to be edit
    a set of config options. Track onload/onchange events

---
+++ packages/commons/src/views/Editor/Editor.tsx
@@ -0,0 +1,73 @@
+import Leafwriter from '@cwrc/leafwriter/src/index';
+import { Box } from '@mui/material';
+import { useActions, useAppState } from '@src/overmind';
+import React, { useEffect, useRef, type FC } from 'react';
+
+const Editor: FC = () => {
+  const { user } = useAppState().auth;
+  const { leafWriter } = useAppState().editor;
+  const { resource } = useAppState().storage;
+
+  const { getLincsAauthenticationToken } = useActions().auth;
+  const { addToRecentDocument } = useActions().storage;
+  const { setIsDirty, setLeafWriter } = useActions().editor;
+
+  const divEl = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (divEl.current && !leafWriter && user && resource?.content) {
+      const leafWriter = new Leafwriter(divEl.current, {
+        document: {
+          url: resource.url,
+          xml: resource.content ?? '',
+        },
+        settings: {
+          credentials: { nssiToken: getLincsAauthenticationToken },
+          lookups: {
+            authorities: [
+              // ['cwrc', { config: { entityCollectionsUrl: '', entityFormsRoot: '', collectionsRoot: '' } }],
+              'viaf',
+              ['wikidata', { enabled: true }],
+              'dbpedia',
+              ['getty', { entities: ['person', ['place', { enabled: false }]] }],
+              'lgpn',
+              ['geonames', { config: { username: 'cwrcgeonames' } }],
+            ],
+            showNoLinkButton: true,
+            showCreateNewButton: false,
+            showEditButton: false,
+            serviceType: 'custom',
+          },
+          legacy: {
+            cwrcRootUrl: './', // '.' | './'
+            helpUrl: 'https://cwrc.ca/CWRC-Writer_Documentation/',
+            nerveUrl: 'https://localhost/nerve/',
+            proxyCssEndpoint: '/schema/css/',
+            proxyXmlEndpoint: '/schema/xml/',
+          },
+        },
+        user: {
+          avatar_url: user.avatar_url,
+          email: user.email,
+          name: `${user?.firstName} ${user?.lastName}`,
+          uri: user?.url,
+        },
+      });
+
+      leafWriter.isDirty.subscribe((value) => setIsDirty(value));
+      leafWriter.onLoad.subscribe(({ schemaName }) => {
+        addToRecentDocument({ ...resource, schemaName });
+      });
+
+      setLeafWriter(leafWriter);
+
+      // return () => {
+      //   leafWriter.isDisrty.unsubscribe();
+      // };
+    }
+  }, []);
+
+  return <Box ref={divEl} sx={{ height: 'calc(100vh - 48px)', overflow: 'hidden' }} />;
+};
+
+export default Editor;
--- packages/commons/src/views/Editor/index.tsx
+++ packages/commons/src/views/Editor/index.tsx
@@ -1,16 +1,16 @@
-import { loadDocument, saveDocument } from '@cwrc/leafwriter-storage-service';
-import type { Types } from '@cwrc/leafwriter';
-import { Backdrop, LinearProgress } from '@mui/material';
+import { loadDocument } from '@cwrc/leafwriter-storage-service';
+// const Leafwriter = React.lazy(() => import('@cwrc/leafwriter'));
 import LoadingMask from '@src/components/loadingMask';
 import Page from '@src/components/Page';
 import { usePermalink } from '@src/hooks/usePermalink';
 import { useActions, useAppState } from '@src/overmind';
-import React, { FC, Suspense, useEffect, useState } from 'react';
+import React, { useEffect, type FC } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router';
-import type { IAnnotationUserProfile, Resource } from '../../@types/types';
+import Editor from './Editor';
+import TopBar from './topBar';
 
-const Leafwriter = React.lazy(() => import('@cwrc/leafwriter'));
+const EditView: FC = () => {
   const { userState } = useAppState().auth;
   const { resource } = useAppState().storage;
 
@@ -18,153 +18,76 @@ const Leafwriter = React.lazy(() => import('@cwrc/leafwriter'));
   const { getStorageProviderAuth, openStorageDialog, setResource } = useActions().storage;
   const { showAlertDialog } = useActions().ui;
 
-const Editor: FC = () => {
   const { t } = useTranslation();
   const navigate = useNavigate();
 
-  const [userProfile, setUserProfile] = useState<IAnnotationUserProfile | undefined>();
+  const { getResourceFromPermalink } = usePermalink();
 
   useEffect(() => {
-    if (resource) {
-      setPermalink(resource);
-      addToRecentDocument(resource);
-    }
+    window.addEventListener('keydown', onKeydownHandle);
+    return () => window.removeEventListener('keydown', onKeydownHandle);
   }, []);
 
   useEffect(() => {
-    if (userAuthenticated === 'authenticating') return;
-    if (!userAuthenticated) return navigate('/', { replace: true });
+    if (userState === 'AUTHENTICATING') return;
+    if (userState === 'UNAUTHENTICATED') return navigate('/', { replace: true });
 
-    const profile = getUserProfile();
-    if (!profile) return navigate('/');
-    setUserProfile(profile);
+    if (!resource) loadDocumentFromPermalink();
+  }, [userState]);
 
-    resource ? setPermalink(resource) : checkPermalink();
-  }, [userAuthenticated]);
-
-  useEffect(() => {
-    if (resource) {
-      if (!getUserProfile()) return navigate('/', { replace: true });
-
-      setPermalink(resource);
-      addToRecentDocument(resource);
-    } else {
-      checkPermalink();
-    }
-  }, [resource]);
-
-  const checkPermalink = async () => {
-    const permalink = parsePermalink();
-    if (!permalink) return navigate('/', { replace: true });
-
-    if ('error' in permalink || !permalink.valid || !permalink.resource) {
-      showMessageDialog({
-        title: 'Warning',
-        message: permalink.error,
-        onClose: () => navigate('/', { replace: true }),
-      });
-      return;
-    }
-
-    if (!permalink.resource.filename) return navigate('/', { replace: true });
-
-    loadDocumentFromPermalink(permalink.resource);
-  };
-
-  const loadDocumentFromPermalink = async (resource: Resource) => {
-    if (!resource.provider) return;
+  const loadDocumentFromPermalink = async () => {
+    const resource = getResourceFromPermalink();
+    if (!resource) return showErrorMessage('Resource not found');
+    if (!resource.provider) return showErrorMessage('Provider not found');
 
     const providerAuth = getStorageProviderAuth(resource.provider);
-    if (!providerAuth) return;
+    if (!providerAuth) return showErrorMessage('Provider not found');
 
     const document = await loadDocument(providerAuth, resource);
-    if (!document || 'error' in document || !document.content || !document.url) {
-      return;
-    }
+    if ('error' in document) return showErrorMessage(document.error);
 
     setResource(document);
-    setPermalink(document);
-    addToRecentDocument(document);
   };
 
-  const handleLoadRequest = () => {
-    openStorageDialog({ source: 'cloud', resource: undefined, type: 'load' });
+  const showErrorMessage = (error?: string) => {
+    showAlertDialog({
+      type: 'error',
+      message: error ?? 'Something went wrong.',
+      onClose: () => navigate('/', { replace: true }),
+    });
   };
 
-  const handleSaveRequest = async (document: Types.LWDocument, saveAs?: boolean) => {
-    if (document.file) setResource(document.file);
+  const onKeydownHandle = (event: KeyboardEvent) => {
+    if (!event.metaKey) return;
 
-    if (saveAs) {
-      openStorageDialog({ source: 'cloud', resource: document.file, type: 'save' });
-      return { success: true };
+    let action: 'save' | 'saveAs' | 'load' | '' = '';
+
+    if (event.code === 'KeyS') action = 'save';
+    if (event.shiftKey && event.code === 'KeyS') action = 'saveAs';
+    if (event.code === 'KeyO') action = 'load';
+
+    if (action === '') return;
+
+    event.preventDefault();
+    event.stopPropagation();
+
+    if (action === 'saveAs') return editor.saveAs();
+    if (action === 'save') return editor.save();
+    if (action === 'load') {
+      return openStorageDialog({
+        source: 'cloud',
+        resource: undefined,
+        type: 'load',
+      });
     }
-
-    if (!document.file?.provider) return { success: false, reason: 'Provider not found' };
-    const providerAuth = getStorageProviderAuth(document.file.provider);
-    if (!providerAuth) return { success: false, reason: 'Provider token not found' };
-
-    const response = await saveDocument(providerAuth, document.file, true);
-    if ('error' in response) {
-      return { success: false, reason: response.error };
-    }
-
-    return { success: true, hash: response.hash };
   };
-
-  const Progress = () => (
-    <Backdrop sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }} open={true}>
-      <LinearProgress sx={{ width: '100%' }} />
-    </Backdrop>
-  );
 
   return (
     <Page title={t('home:homepage')}>
-      {!resource || !userProfile ? (
-        <Progress />
-      ) : (
-        <Suspense fallback={<Progress />}>
-          <Leafwriter
-            document={{
-              file: resource,
-              url: resource.url ?? '',
-              xml: resource.content ?? '',
-            }}
-            editor={{
-              credentials: { nssiToken: getLincsAauthenticationToken },
-              lookups: lookUpsConfig,
-              legacy: {
-                cwrcRootUrl: './', // '.' | './'
-                helpUrl: 'https://cwrc.ca/CWRC-Writer_Documentation/',
-                nerveUrl: 'https://localhost/nerve/',
-                proxyCssEndpoint: '/schema/css/',
-                proxyXmlEndpoint: '/schema/xml/',
-              },
-            }}
-            onLoadRequest={handleLoadRequest}
-            onSaveRequest={handleSaveRequest}
-            user={{ ...userProfile, signOut }}
-          />
-        </Suspense>
-      )}
+      <TopBar />
       {!resource ? <LoadingMask /> : <Editor />}
     </Page>
   );
 };
 
-export default Editor;
-
-const lookUpsConfig: Types.ILookupsConfig = {
-  authorities: [
-    // ['cwrc', { config: { entityCollectionsUrl: '', entityFormsRoot: '', collectionsRoot: '' } }],
-    'viaf',
-    ['wikidata', { enabled: true }],
-    'dbpedia',
-    ['getty', { entities: ['person', ['place', { enabled: false }]] }],
-    'lgpn',
-    ['geonames', { config: { username: 'cwrcgeonames' } }],
-  ],
-  showNoLinkButton: true,
-  showCreateNewButton: false,
-  showEditButton: false,
-  serviceType: 'custom',
-};
+export default EditView;
