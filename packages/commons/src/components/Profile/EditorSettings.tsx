f198f079d517d380e5242f6c999e6c2a11bd3b7e
Luciano Frizzera <lucaju@me.com>
2022-06-19 18:43:09 -0400

    feat(commons): 🎸 Add ability to open Leaf-Writer settings dialog
    also chnges settings icon

--- packages/commons/src/components/Profile/EditorSettings.tsx
+++ packages/commons/src/components/Profile/EditorSettings.tsx
@@ -1,20 +1,27 @@
-import SettingsIcon from '@mui/icons-material/Settings';
+import TuneIcon from '@mui/icons-material/Tune';
 import { ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import { useAppState } from '@src/overmind';
 import React, { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 
-const EditorSettings: FC = () => {
+interface Props {
+  handleClose: () => void;
+}
+
+const EditorSettings: FC<Props> = ({ handleClose }) => {
+  const { leafWriter } = useAppState().editor;
   const { t } = useTranslation();
 
   const handleClick = () => {
-    console.log('Open Settings Dialog');
+    handleClose();
+    leafWriter?.showSettingsDialog();
   };
 
   return (
     <ListItem disableGutters>
       <ListItemButton onClick={handleClick}>
         <ListItemIcon sx={{ minWidth: 40 }}>
-          <SettingsIcon fontSize="small" />
+          <TuneIcon fontSize="small" />
         </ListItemIcon>
         <ListItemText id="settings" primary={t('settings')} sx={{ textTransform: 'capitalize' }} />
       </ListItemButton>
