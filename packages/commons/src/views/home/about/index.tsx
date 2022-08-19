c7395946ba01a90c978d689046d444369791b8d7
Luciano Frizzera <lucaju@me.com>
2022-08-18 23:03:46 -0400

    fix(commons): 🐛 Theme: align primary color with the brand style guide
    Also add a light background color to the abour section

--- packages/commons/src/views/home/about/index.tsx
+++ packages/commons/src/views/home/about/index.tsx
@@ -1,21 +1,35 @@
-import { Box, Container, Grid } from '@mui/material';
-import React, { FC } from 'react';
+import { Box, Container, Grid, useTheme, rgbToHex } from '@mui/material';
+import React, { FC, useMemo } from 'react';
 import About from './About';
 import Team from './Team';
+import chroma from 'chroma-js';
 
-const AboutSction: FC = () => (
-  <Box py={10} px={2}>
-    <Container maxWidth="lg">
-      <Grid container columnSpacing={12} rowSpacing={4} mb={5}>
-        <Grid item xs={12} sm={7} md={8}>
-          <About />
+const AboutSction: FC = () => {
+  const { mode, primary } = useTheme().palette;
+
+  const backgroundColor = useMemo(
+    () =>
+      chroma
+        .hex(rgbToHex(primary.main))
+        .alpha(mode === 'dark' ? 0.04 : 0.06)
+        .hex(),
+    [mode]
+  );
+
+  return (
+    <Box py={10} px={2} sx={{ backgroundColor }}>
+      <Container maxWidth="lg">
+        <Grid container columnSpacing={12} rowSpacing={4} mb={5}>
+          <Grid item xs={12} sm={7} md={8}>
+            <About />
+          </Grid>
+          <Grid item xs={12} sm={5} md={4}>
+            <Team />
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={5} md={4}>
-          <Team />
-        </Grid>
-      </Grid>
-    </Container>
-  </Box>
-);
+      </Container>
+    </Box>
+  );
+};
 
 export default AboutSction;
