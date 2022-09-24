8b591b29ce3514307579b64894ad84a741fd72ac
Luciano Frizzera <lucaju@me.com>
2022-09-23 21:23:46 -0400

    feat(commons): 🎸 Improve UX access to template
    move templates to a json file. Create a dialog to select a template.
    
    ✅ Closes: #77

--- packages/commons/src/views/home/storagePanel/Templates.tsx
+++ packages/commons/src/views/home/storagePanel/Templates.tsx
@@ -1,14 +1,19 @@
-import { Button, Divider, Stack, Typography, useMediaQuery, useTheme } from '@mui/material';
+import { Button, Divider, Icon, Stack, Typography, useMediaQuery, useTheme } from '@mui/material';
 import { useActions, useAppState } from '@src/overmind';
 import { getIcon } from '@src/utilities';
 import React, { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router';
 
-const Templates: FC = () => {
+const MAX_TEMPLATES_SHOW = 4;
+
+export const Templates: FC = () => {
   const navigate = useNavigate();
   const { templates } = useAppState().storage;
+
   const { loadTemplate, setResource } = useActions().storage;
+  const { openDialog } = useActions().ui;
+
   const { t } = useTranslation();
   const { breakpoints, palette } = useTheme();
   const isMobile = useMediaQuery(breakpoints.down('sm'));
@@ -19,13 +24,17 @@ const Templates: FC = () => {
     navigate(`/edit?template=${title}`, { replace: true });
   };
 
+  const handleOpenTemplates = () => {
+    openDialog({ type: 'templates' });
+  };
+
   return (
     <Stack
       direction={isMobile ? 'column' : 'row'}
       spacing={isMobile ? 1 : 3}
       alignItems="center"
       sx={{
-        width: isMobile ? '90vw' : 600,
+        width: isMobile ? '90vw' : 700,
         py: 0.75,
         px: 2,
         backgroundColor: palette.mode === 'dark' ? palette.grey[900] : palette.grey[50],
@@ -48,39 +57,53 @@ const Templates: FC = () => {
       />
       <Stack
         direction={isMobile ? 'row' : 'row'}
-        justifyContent={isMobile ? 'flex-start' : 'center'}
+        justifyContent="flex-start"
         spacing={3}
         overflow="auto"
         width="100%"
         pb={isMobile ? 1 : 0}
         px={isMobile ? 1 : 0}
       >
-        {templates.map(({ icon, title, url }) => {
-          const Icon = getIcon(icon);
-          return (
-            <Button
-              key={title}
-              color="primary"
-              onClick={() => handleClick(title, url)}
-              size="small"
-              startIcon={<Icon fontSize="inherit" />}
-              sx={{
-                color: 'inherit',
-                px: 1,
-                '&:hover': {
-                  color: palette.primary.light,
-                  backgroundColor: 'transparent',
-                },
-              }}
-              variant="text"
-            >
-              {title}
-            </Button>
-          );
-        })}
+        {templates
+          .filter((_t, index) => index < MAX_TEMPLATES_SHOW)
+          .map(({ icon, title, url }) => {
+            return (
+              <Button
+                key={url}
+                onClick={() => handleClick(title, url)}
+                size="small"
+                startIcon={<Icon component={getIcon(icon ?? 'blankPage')} fontSize="inherit" />}
+                sx={{
+                  px: 1,
+                  '&:hover': {
+                    color: palette.primary.light,
+                    backgroundColor: 'transparent',
+                  },
+                }}
+              >
+                {title}
+              </Button>
+            );
+          })}
+        {templates.length > MAX_TEMPLATES_SHOW && (
+          <Button
+            color="primary"
+            onClick={handleOpenTemplates}
+            size="small"
+            sx={{
+              color: 'inherit',
+              px: 1,
+              '&:hover': {
+                color: palette.primary.light,
+                backgroundColor: 'transparent',
+              },
+            }}
+            variant="text"
+          >
+            {t('more')}
+          </Button>
+        )}
       </Stack>
     </Stack>
   );
 };
-
-export default Templates;
