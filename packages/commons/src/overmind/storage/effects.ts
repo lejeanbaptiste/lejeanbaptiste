3248e3fee1fa2c2d60676bc1d003609a3875e390
Luciano Frizzera <lucaju@me.com>
2022-09-23 21:30:55 -0400

    fix(commons): 🐛 Improve API to save/retrieve from localstorage

--- packages/commons/src/overmind/storage/effects.ts
+++ packages/commons/src/overmind/storage/effects.ts
@@ -13,4 +13,14 @@ export const api = {
     const response = await axios.get(url);
     return response.data;
   },
+  saveToLocalStorage: (key: string, value: unknown) => {
+    localStorage.setItem(key, JSON.stringify(value));
+  },
+  getFromLocalStorage: (key: string) => {
+    const value = localStorage.getItem(key);
+    return value ? JSON.parse(value) : null;
+  },
+  removeFromLocalStorage: (key: string) => {
+    localStorage.removeItem(key);
+  },
 };
