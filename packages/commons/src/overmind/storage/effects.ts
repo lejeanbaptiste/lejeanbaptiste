d6c1c12e6e287c31c47414ac0bd1a4fbcaf26c5b
Luciano Frizzera <lucaju@me.com>
2022-10-12 11:53:49 -0400

    refactor(commons): 💡 Storage effects: Rename, coalesce, and add types to functions

--- packages/commons/src/overmind/storage/effects.ts
+++ packages/commons/src/overmind/storage/effects.ts
@@ -1,29 +1,22 @@
 import axios from 'axios';
 
 export const api = {
-  async loadTemplates() {
-    const response = await axios.get('./content/templates.json');
-    return response.data;
+  async loadCollection(collection: string) {
+    const { data } = await axios.get<ISample[]>(`./content/${collection}.json`);
+    return data;
   },
-  async loadSampleDocuments() {
-    const response = await axios.get('./content/sampleDocuments.json');
-    return response.data;
   },
-  async loadTemplate(url: string) {
-    const response = await axios.get(url);
-    return response.data;
-  },
-  saveToLocalStorage: (key: string, value: unknown) => {
+  saveToLocalStorage<T = unknown>(key: string, value: T) {
     const stringfiedValue = typeof value === 'string' ? value : JSON.stringify(value);
     localStorage.setItem(key, stringfiedValue);
   },
-  getFromLocalStorage: (key: string) => {
+  getFromLocalStorage<T = string>(key: string): T | null {
     const value = localStorage.getItem(key);
     if (!value) return null;
     const parsedValue = value.startsWith('{') || value.startsWith('[') ? JSON.parse(value) : value;
     return parsedValue;
   },
-  removeFromLocalStorage: (key: string) => {
+  removeFromLocalStorage(key: string) {
     localStorage.removeItem(key);
   },
 };
