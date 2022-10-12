484201efa04d179047dc8bb85cc37041810f54cb
Luciano Frizzera <lucaju@me.com>
2022-10-12 13:02:15 -0400

    refactor(commons): 💡 rewrite sample collection

--- packages/commons/src/overmind/storage/state.ts
+++ packages/commons/src/overmind/storage/state.ts
@@ -4,14 +4,12 @@ import type { Resource, StorageDialogState } from '@src/types';
 type State = {
   recentDocuments: Resource[];
   resource?: Resource;
-  sampleDocuments?: { title: string; url: string }[];
   storageDialogState: StorageDialogState;
   storageProviders: StorageProviderName[];
 };
 
 export const state: State = {
   recentDocuments: [],
-  sampleDocuments: [],
   storageDialogState: { open: false },
   storageProviders: [],
 };
