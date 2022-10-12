e05e8ca2646de924492594c7e5024e36830a3601
Luciano Frizzera <lucaju@me.com>
2022-10-12 11:08:58 -0400

    style(commons): 💄 server: formating code

--- packages/commons/server/dev/index.ts
+++ packages/commons/server/dev/index.ts
@@ -1,4 +1,4 @@
-import { Express } from 'express';
+import type { Express } from 'express';
 import kleur from 'kleur';
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
