366b767cf71ba809e8fbf6997b2265c39fa23710
Luciano Frizzera <lucaju@me.com>
2022-10-12 17:33:15 -0400

    feat(commons): 🎸 Show filename at the HTML title when editing a file
    Replace react-helmet for react-helmet-async

--- packages/commons/src/layouts/Page.tsx
+++ packages/commons/src/layouts/Page.tsx
@@ -1,15 +1,24 @@
+import { useAppState } from '@src/overmind';
 import React, { forwardRef } from 'react';
-import { Helmet } from 'react-helmet';
+// import { Helmet } from 'react-helmet';
+import { Helmet } from 'react-helmet-async';
 
 interface PageProps {
   title?: string;
 }
 
-export const Page = forwardRef<PageProps, any>(({ children, title = '', ...rest }, ref) => {
+const PAGE_TITLE = 'LEAF-Writer Commons';
+
+export const Page = forwardRef<PageProps, any>(({ children, title, ...rest }, ref) => {
+  const { resource } = useAppState().storage;
+
+  title = title ? title : PAGE_TITLE;
+  title = resource?.filename ? `${resource.filename} - ${PAGE_TITLE}` : PAGE_TITLE;
+
   return (
     <div ref={ref} {...rest}>
       <Helmet>
-        <title>LEAF-Writer Commons</title>
+        <title>{title}</title>
       </Helmet>
       {children}
     </div>
