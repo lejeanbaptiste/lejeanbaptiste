118bbc4420de174c18f02dcbfe233878e0ae001d
Luciano Frizzera <lucaju@me.com>
2022-12-02 21:29:41 -0500

    refactor(commons): 💡 move cloud disable message to a hook

--- packages/commons/src/hooks/index.ts
+++ packages/commons/src/hooks/index.ts
@@ -2,5 +2,6 @@ export { useCookieConsent } from './cookieConsent/useCookieConsent';
 export { useAnalytics } from './useAnalytics';
 export { useDialog } from './useDialog';
 export { useKeyboardShortcut } from './useKeyboardShortcuts';
+export { useMessage } from './useMessage';
 export { useNotifier } from './useNotifier';
 export { usePermalink } from './usePermalink';
---
+++ packages/commons/src/hooks/useMessage.tsx
@@ -0,0 +1,37 @@
+import { Typography } from '@mui/material';
+import { useAppState } from '@src/overmind';
+import React, { useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+
+export const useMessage = () => {
+  const { userState } = useAppState().auth;
+  const { storageProviders } = useAppState().providers;
+
+  const { t } = useTranslation('storage');
+
+  const cloudDisabledMessage = useMemo(() => {
+    if (userState !== 'AUTHENTICATED') {
+      return `${t('messages:you_must_sign_in_to_open_and_save_document_from_the_cloud')}`;
+    }
+
+    if (!storageProviders.some((provider) => provider.service?.isStorageProvider)) {
+      return (
+        <>
+          <Typography paragraph variant="caption">
+            {t(
+              'messages:you_must_sign_in_witn_a_storage_provider_to_open_and_save_document_from_the_cloud'
+            )}
+            .
+          </Typography>
+          <Typography variant="caption">
+            {t('messages:link_to_storage_provider_instructions')}.
+          </Typography>
+        </>
+      );
+    }
+  }, [userState, storageProviders]);
+
+  return {
+    cloudDisabledMessage,
+  };
+};
