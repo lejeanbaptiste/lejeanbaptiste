bcebdbccb571137c4fe8468f69597069b81e3987
Luciano Frizzera <lucaju@me.com>
2023-03-25 15:31:38 -0400

    feat(validator): 🎸 Change API to coalesce results, allows for textNode validation, and fix bugs
    When speculativelly valdiating, possible tags have an extra property
    'invalid' (boolean) intead of a second array listing speculative
    validated tags. This also fix a bug that prevent validation on
    differents modes of speculations (before, after, ...) .  Another change
    is the addition of textNodes as possible "tags" events .
    
    BREAKING CHANGE: 🧨 Changes on the API call and results. Check documentation.

--- packages/validator/src/sharedTypes.ts
+++
@@ -1,8 +0,0 @@
-export type ElementType = 'node' | 'tag' | 'attribute' | 'attributeValue';
-export interface ElementDetail {
-  name: string;
-  type: ElementType;
-  documentation?: string;
-  fullName?: string;
-  ns?: string;
-}
