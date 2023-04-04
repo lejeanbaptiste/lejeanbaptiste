ab416897716729d33d11267d4c1c7fbb30677853
Luciano Frizzera <lucaju@me.com>
2023-04-04 00:37:06 -0400

    fix(core): 🐛 Improve localSotage api to receive generic Type

--- packages/core/src/overmind/editor/effects.ts
+++ packages/core/src/overmind/editor/effects.ts
@@ -10,22 +10,48 @@ export const api = (() => {
     setLookupsDefaults: (value: LookupsProps) => {
       lookupsDefaults = value;
     },
-    saveToLocalStorage: (key: string, value: unknown) => {
-      localStorage.setItem(key, JSON.stringify(value));
+
+    /**
+     * It saves a value to local storage, but if the value is not a string, it converts it to a string
+     * before saving it
+     * @param {string} key - string - The key to save the value under.
+     * @param {unknown} value - The value to be stored in local storage.
+     */
+    saveToLocalStorage<T = unknown>(key: string, value: T) {
+      const stringfiedValue = typeof value === 'string' ? value : JSON.stringify(value);
+      localStorage.setItem(key, stringfiedValue);
     },
-    getFromLocalStorage: (key: string) => {
+
+    /**
+     * It gets a value from local storage, and if it's a JSON string, it parses it into an object
+     * @param {string} key - The key to store the value under.
+     * @returns The value of the key in localStorage.
+     */
+    getFromLocalStorage<T = string>(key: string): T | null {
       const value = localStorage.getItem(key);
-      if (!value) return;
+      if (!value) return null;
 
       try {
         const object = JSON.parse(value);
-        return object;
+        return object as T;
       } catch (error) {
-        return value;
+        return value as T;
       }
     },
+
+    /**
+     * It removes a key from local storage
+     * @param {string} key - The key to be used to store the data in local storage.
+     */
     removeFromLocalStorage: (key: string) => {
       localStorage.removeItem(key);
+    },
+
+    /**
+     * It deletes all keys from local storage
+     */
+    clearLocalStorage() {
+      localStorage.clear();
     },
   };
 })();
