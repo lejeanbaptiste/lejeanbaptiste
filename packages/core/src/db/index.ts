3d90977d9fccd87b39fb653f102e333d08286a9a
Luciano Frizzera <lucaju@me.com>
2023-04-09 22:02:12 -0400

    feat(core): 🎸 Expose convenient functions to clear cache and delete inxdedDB used by 'leafwriter'

--- packages/core/src/db/index.ts
+++ packages/core/src/db/index.ts
@@ -1,6 +1,10 @@
 import Dexie, { Table } from 'dexie';
 import { AuthorityService } from '../dialogs';
 import type { Schema } from '../types';
+import {
+  clearCache as clearCacheValidator,
+  deleteDb as deleteDbValidator,
+} from '@cwrc/leafwriter-validator/db';
 
 export interface SuspendedDocument {
   content: string;
@@ -29,3 +33,24 @@ export class DexieDB extends Dexie {
 }
 
 export const db = new DexieDB();
+
+export const clearCache = async () => {
+  await clearCacheValidator();
+  await db.suspendedDocuments
+    .clear()
+    .catch(() => new Error('Clear `suspendedDocuments` table: Something went wrong.'));
+  await db.customSchemas
+    .clear()
+    .catch(() => new Error('Clear `customSchemas` table: Something went wrong.'));
+  await db.authorityServices
+    .clear()
+    .catch(() => new Error('Clear `authorityServices` table: Something went wrong.'));
+  await db.doNotDisplayDialogs
+    .clear()
+    .catch(() => new Error('Clear `doNotDisplayDialogs` table: Something went wrong.'));
+};
+
+export const deleteDb = async () => {
+  await deleteDbValidator();
+  return await db.delete().catch(() => new Error('Something went wrong.'));
+};
