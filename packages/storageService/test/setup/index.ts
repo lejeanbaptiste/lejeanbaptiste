ffc7126c08b9955de10e0c95fb1e7248f7599ddf
Luciano Frizzera <lucaju@me.com>
2023-04-17 21:27:41 -0400

    test(storage service): 💍 Improve tests

--- packages/storageService/test/setup/index.ts
+++ packages/storageService/test/setup/index.ts
@@ -1,5 +1,9 @@
 // ! Configuring Your Testing Environment
 // https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html
-// globalThis.IS_REACT_ACT_ENVIRONMENT = true;
+//@ts-ignore
+globalThis.IS_REACT_ACT_ENVIRONMENT = true;
 
 import '@testing-library/jest-dom';
+
+const noop = () => {};
+Object.defineProperty(window, 'scrollTo', { value: noop, writable: true });
