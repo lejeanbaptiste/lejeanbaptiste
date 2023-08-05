d0d9f198d25166e3a2a83ad464fef66b1d612ade
Luciano Frizzera <lucaju@me.com>
2023-08-04 22:58:17 -0400

    style(core): 💄 formating

--- packages/core/src/js/utilities.ts
+++ packages/core/src/js/utilities.ts
@@ -148,7 +148,7 @@ class Utilities {
           skipWhitespace === false ||
           (skipWhitespace &&
             node.textContent?.match(
-              /^[\f\n\r\t\v\u00a0\u1680\u2000-\u200a\u2028\u2029\u202f\u205f\u3000]*$/
+              /^[\f\n\r\t\v\u00a0\u1680\u2000-\u200a\u2028\u2029\u202f\u205f\u3000]*$/,
             ) === null)
         ) {
           return NodeFilter.FILTER_ACCEPT;
@@ -174,7 +174,7 @@ class Utilities {
           skipWhitespace === false ||
           (skipWhitespace &&
             node.textContent?.match(
-              /^[\f\n\r\t\v\u00a0\u1680\u2000-\u200a\u2028\u2029\u202f\u205f\u3000]*$/
+              /^[\f\n\r\t\v\u00a0\u1680\u2000-\u200a\u2028\u2029\u202f\u205f\u3000]*$/,
             ) === null)
         ) {
           return NodeFilter.FILTER_ACCEPT;
