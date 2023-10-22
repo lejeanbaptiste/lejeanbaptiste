c4092b2db1df39da07ec2cbe17971a0e87a58c7d
Luciano Frizzera <lucaju@me.com>
2023-10-21 20:17:35 -0400

    test(validator): 💍 testEnvironment: workaraound to make 'structuredClone' work on jsdom

--- packages/validator/jest.config.ts
+++ packages/validator/jest.config.ts
@@ -145,7 +145,7 @@ export default {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: 'jsdom',
+  testEnvironment: './test/setup/FixJSDOMEnvironment.ts', // * workaround to add 'structuredClone' to 'jest-environment-jsdom v.29.6.2'. When it gets fixed, we should return to use 'jsdom'. 
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
