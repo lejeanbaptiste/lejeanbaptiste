42b7269782344ceddd0a1889ff2499cdd1338bd1
Luciano Frizzera <lucaju@me.com>
2023-10-22 17:42:55 -0400

    style(commons): 💄 formating

--- packages/commons/webpack.config.ts
+++ packages/commons/webpack.config.ts
@@ -1,5 +1,4 @@
 import path from 'path';
-
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 import CopyWebpackPlugin from 'copy-webpack-plugin';
 import { EsbuildPlugin } from 'esbuild-loader';
