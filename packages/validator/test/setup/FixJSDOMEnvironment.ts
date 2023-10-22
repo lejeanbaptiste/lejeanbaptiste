2bc30f2567d7c11f21f2246216212b654309b378
Luciano Frizzera <lucaju@me.com>
2023-10-21 22:01:01 -0400

    refactor(validator): 💡 linting, formating, typing

--- packages/validator/test/setup/FixJSDOMEnvironment.ts
+++ packages/validator/test/setup/FixJSDOMEnvironment.ts
@@ -1,4 +1,3 @@
-
 import JSDOMEnvironment from 'jest-environment-jsdom';
 
 // https://github.com/facebook/jest/blob/v29.4.3/website/versioned_docs/version-29.4/Configuration.md#testenvironment-string
