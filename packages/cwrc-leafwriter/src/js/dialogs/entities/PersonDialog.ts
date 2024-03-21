fb1a61a6e49ea72c2be29eb4f00813578365a06e
Luciano Frizzera <lucaju@me.com>
2024-03-20 21:04:24 -0400

    fix(leafwriter): 🐛 dialogs: Change 'markups' to 'attributes'
    ✅ Closes: #181

--- packages/cwrc-leafwriter/src/js/dialogs/entities/PersonDialog.ts
+++ packages/cwrc-leafwriter/src/js/dialogs/entities/PersonDialog.ts
@@ -77,7 +77,7 @@ class PersonDialog implements SchemaDialog {
           </div>
 
           <div class="attributeSelector">
-            <h3 style="border-bottom: 1px solid #ddd; padding-bottom: 4px;">Markups</h3>
+            <h3 style="border-bottom: 1px solid #ddd; padding-bottom: 4px;">Attributes</h3>
             <ul></ul>
           </div>
         </div>
