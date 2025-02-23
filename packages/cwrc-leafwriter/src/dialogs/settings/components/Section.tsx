0125243ca2e03f91b9ef7265ac06c40a79789549
Luciano Frizzera <lucaju@me.com>
2025-02-23 12:56:30 -0500

    refactor(leafwriter): 💡 settings panel: UI and components adjustments

--- packages/cwrc-leafwriter/src/dialogs/settings/components/Section.tsx
+++ packages/cwrc-leafwriter/src/dialogs/settings/components/Section.tsx
@@ -1,20 +1,29 @@
-import { Box, List, Stack, Typography } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
 
 export interface SectionProps extends React.PropsWithChildren {
+  endDecorator?: React.ReactNode;
   id: string;
-  title: string;
+  title: React.ReactNode;
 }
 
-export const Section = ({ id, title, children }: SectionProps) => {
+export const Section = ({ children, endDecorator, id, title }: SectionProps) => {
   return (
     <Stack id={id}>
-      <Box mt={1.5} px={2} py={0.5} borderBottom="1px solid">
+      <Stack
+        direction="row"
+        justifyContent="space-between"
+        mt={1.5}
+        px={2}
+        py={0.5}
+        borderBottom="1px solid"
+      >
         <Typography variant="subtitle1" sx={{ textTransform: 'capitalize' }}>
           {title}
         </Typography>
-      </Box>
+        {endDecorator}
+      </Stack>
       <Stack px={1} spacing={1}>
-        <List dense>{children}</List>
+        {children}
       </Stack>
     </Stack>
   );
--- packages/cwrc-leafwriter/src/dialogs/settings/index.tsx
+++ packages/cwrc-leafwriter/src/dialogs/settings/index.tsx
@@ -6,19 +6,12 @@ import type { IDialog } from '../type';
 import { Header } from './Header';
 import { SideMenu, type MenuItemProps } from './SideMenu';
 import { Section } from './components';
-import { Authorities, Editor, Interface, MarkupPanel, Reset } from './sections';
+import { Header } from './header';
+import { SideMenu } from './side-menu';
 
 export const SettingsDialog = ({ id, onClose, open = false }: IDialog) => {
   const { isReadonly, settings } = useAppState().editor;
   const { t } = useTranslation();
-
-  const menuItems: MenuItemProps[] = [
-    { id: 'interface', label: t('LW.commons.interface') },
-    { id: 'editor', label: t('LW.commons.editor') },
-    { id: 'authorities', label: t('LW.commons.authorities'), hide: isReadonly },
-    { id: 'markup-panel', label: t('LW.markup panel'), hide: isReadonly },
-    { id: 'reset', label: t('LW.commons.reset'), hide: isReadonly },
-  ];
 
   const handleClose = () => onClose && onClose(id);
 
@@ -27,13 +20,22 @@ export const SettingsDialog = ({ id, onClose, open = false }: IDialog) => {
       aria-labelledby="settings-title"
       container={document.getElementById(`${settings?.container}`)}
       fullWidth
-      maxWidth="sm"
+      maxWidth="md"
       onClose={handleClose}
       open={open}
     >
       <Header onClose={handleClose} />
       <Stack direction="row" overflow="hidden" px={1.5}>
-        <SideMenu items={menuItems} />
+        <SideMenu
+          items={[
+            { id: 'interface', label: t('LW.commons.interface') },
+            { id: 'editor', label: t('LW.commons.editor') },
+            { id: 'authorities', label: t('LW.commons.authorities'), hide: isReadonly },
+            { id: 'entityLookups', label: t('LW.commons.Entity Lookups'), hide: isReadonly },
+            { id: 'markup-panel', label: t('LW.commons.markup panel'), hide: isReadonly },
+            { id: 'reset', label: t('LW.commons.reset'), hide: isReadonly },
+          ]}
+        />
         <DialogContent>
           <Stack component={motion.div} layout spacing={3}>
             <Section id="interface" title={t('LW.commons.interface')}>
