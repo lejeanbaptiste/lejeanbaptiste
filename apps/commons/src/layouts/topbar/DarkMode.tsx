c3a10f2737a0a30ae7302f0de261b748fa9e02e3
Luciano Frizzera <lucaju@me.com>
2025-06-17 21:30:35 -0400

    fix(commons): 🐛 dark mode when not authenticated

--- apps/commons/src/layouts/topbar/DarkMode.tsx
+++ apps/commons/src/layouts/topbar/DarkMode.tsx
@@ -1,18 +1,22 @@
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import { IconButton } from '@mui/material';
+import { useColorScheme } from '@mui/material/styles';
 import { useActions, useAppState } from '@src/overmind';
 import { motion, type Variants } from 'motion/react';
 import { useTranslation } from 'react-i18next';
 
 export const DarkMode = () => {
   const { t } = useTranslation();
-  const { darkMode } = useAppState().ui;
-  const { setDarkMode } = useActions().ui;
+  const { themeAppearance } = useAppState().ui;
+  const { setThemeAppearance } = useActions().ui;
+
+  const { setMode } = useColorScheme();
 
   const switchAppearenceMode = () => {
-    const value = !darkMode;
-    setDarkMode(value);
+    const value = themeAppearance === 'dark' ? 'light' : 'dark';
+    setMode(value);
+    setThemeAppearance(value);
   };
 
   const optionVariants: Variants = {
@@ -33,7 +37,11 @@ export const DarkMode = () => {
       onClick={switchAppearenceMode}
       size="small"
     >
-      {darkMode ? <DarkModeIcon fontSize="inherit" /> : <Brightness7Icon fontSize="inherit" />}
+      {themeAppearance === 'dark' ? (
+        <DarkModeIcon fontSize="inherit" />
+      ) : (
+        <Brightness7Icon fontSize="inherit" />
+      )}
     </IconButton>
   );
 };
