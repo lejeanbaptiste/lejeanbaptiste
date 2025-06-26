d0d128648efd521f353def269fb0d67e539eaa90
Luciano Frizzera <lucaju@me.com>
2025-06-26 10:14:47 -0400

    feat(leafwriter): 🎸 annotation: person: change option type from "both" to "fictionalized"

--- packages/cwrc-leafwriter/src/js/dialogs/entities/PersonDialog.ts
+++ packages/cwrc-leafwriter/src/js/dialogs/entities/PersonDialog.ts
@@ -22,7 +22,7 @@ interface Role {
   value: string;
 }
 
-const personTypeOptions = ['real', 'fictional', 'both'];
+const personTypeOptions = ['real', 'fictional', 'fictionalized'];
 const certaintyOptions = ['high', 'medium', 'low', 'unknown'];
 
 class PersonDialog implements SchemaDialog {
