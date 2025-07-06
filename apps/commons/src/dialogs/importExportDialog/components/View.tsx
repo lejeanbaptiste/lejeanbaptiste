e3244853cc29d033a72e1b7088309fbbecba3d9d
Luciano Frizzera <lucaju@me.com>
2025-07-06 12:12:51 -0400

    fix(leafwriter): 🐛 import dialog: Add link to documentation
    ✅ Closes: #254

--- apps/commons/src/dialogs/importExportDialog/components/View.tsx
+++ apps/commons/src/dialogs/importExportDialog/components/View.tsx
@@ -1,11 +1,14 @@
-import { Stack } from '@mui/material';
+import { Box, Link } from '@mui/material';
+import { Icon } from '@src/icons';
 import type { FileDetail } from '@src/types';
 import { UploadDropBox } from '@src/views/storage';
 import { useAtom, useAtomValue } from 'jotai';
+import { useTranslation } from 'react-i18next';
 import { useConversion } from '../hooks';
 import { fileDetailAtom, isProcessingAtom } from '../store';
 
 export const View = () => {
+  const { t } = useTranslation();
   const { processImportFile: processFile } = useConversion();
 
   const [fileDetail, setFileDetail] = useAtom(fileDetailAtom);
@@ -17,13 +20,22 @@ export const View = () => {
   };
 
   return (
-    <Stack width="100%">
+    <Box width="100%">
       <UploadDropBox
         filename={fileDetail?.file.name}
         isProcessing={isProcessing}
-        width="100%"
         onSelectFile={handleFileSelect}
+        width="100%"
       />
-    </Stack>
+      <Link
+        href="https://www.leaf-vre.org/docs/documentation/leaf-commons/leaf-te-documentation"
+        sx={{ ml: 1 }}
+        target="_blank"
+        variant="body2"
+      >
+        {t('LWC.importExport.Learn more about importing files')}
+        <Icon name="externalLink" fontSize="inherit" sx={{ mb: -0.75, ml: 1, scale: 1.75 }} />
+      </Link>
+    </Box>
   );
 };
